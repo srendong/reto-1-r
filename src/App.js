5a94d65d9ed27361c221689add368c1b8bca91b3
Samuel Rendon G <s.redon.g@gmail.com>
2019-07-02 17:19:24 -0500

    1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Mi primera aplicación</h1>
+      <p>Esta es mi primera aplicación en React, está padrísimo!</p>
+    </div>
+  );
+}
+
+export default App;
